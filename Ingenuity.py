4d8d277ef7a36262aa4756a5b64c0f3149c49641
Kotsomitis Nick <nk.kotsomitis@gmail.com>
2025-08-14 17:12:39 +0300

    Fixed splash screen at startup

--- Ingenuity.py
+++ Ingenuity.py
@@ -6,5 +6,16 @@ from pathlib import Path
 src_path = Path(__file__).parent / "src"
 sys.path.append(str(src_path))
 
+try:
+    import pyi_splash
+    # Update the text on the splash screen
+    pyi_splash.update_text("PyInstaller is a great software!")
+    pyi_splash.update_text("Second time's a charm!")
+    # Close the splash screen
+    pyi_splash.close()
+except ImportError:
+    # pyi_splash is only available in a PyInstaller-built app
+    pass
+
 from src.__main__ import main
 main()
